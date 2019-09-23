0c1051597d26fd4a37ae5c6c52455b316c96f79b
Andrew Hollingworth <andrewhollingworth@Andrews-MacBook-Air.local>
2019-09-22 21:54:39 -0400

    Heroku Redeploy

--- models.js
+++ models.js
@@ -3,22 +3,15 @@ const Sequelize = require('sequelize');
 const db = new Sequelize(
   (process.env.DATABASE_URL || 'postgres://localhost:5432/arcade_db'),
   {
-<<<<<<< HEAD
     database: 'arcade_db',
     dialect: 'postgres',
     define: {
       underscored: true,
     },
-=======
-  database: 'arcade_db',
-  dialect: 'postgres',
-  define: {
-    underscored: true,
->>>>>>> 63fce4c69b463cd3c56ae2c013e77dba1628d253
   },
 );
 
-class User extends Sequelize.Model {}
+class User extends Sequelize.Model { }
 
 User.init({
   username: Sequelize.STRING,
@@ -29,7 +22,7 @@ User.init({
   modelName: 'user',
 });
 
-class Highscore extends Sequelize.Model {}
+class Highscore extends Sequelize.Model { }
 
 Highscore.init({
   scores: Sequelize.INTEGER,
